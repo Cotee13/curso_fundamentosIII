8f259863c0db06dea105aedbbeced1d9baddb0fa
Cotee13 <joselbm020@gmail.com>
2026-08-20 12:55:01 -0700

    Linked list Java

---
+++ PruebaLinkedlist.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebalinkedlist;
+
+/**
+ *
+ * @author José Luis Bustamante Madrid
+ */
+class Student {
+    String name;
+    int score;
+    Student next;
+    
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+        this.next = null;
+    }
+}
+
+class lnkdlst {
+    Student head;
+    public lnkdlst() //Constructor de la clase 
+    {
+        this.head = null;
+    }
+    
+    public void add(String name, int score) {
+        Student newStudent = new Student(name,score);
+        if(head == null) {
+            head = newStudent;
+            return;
+        }
+        Student current = head;
+        while(current.next != null) {
+            current = current.next;
+        }
+        current.next = newStudent;
+    }
+}
+
+public class PruebaLinkedlist {
+
+    public static void main(String[] args) {
+        lnkdlst lista = new lnkdlst();
+        lista.add("Linus", 90);
+        lista.add("Ada", 95);
+        lista.add("Paul", 78);
+    }
+}
