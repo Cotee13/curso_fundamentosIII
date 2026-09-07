063bb642fea26f887f1e6596e25d2a7695b6afd3
Cotee13 <joselbm020@gmail.com>
2026-09-07 12:54:10 -0700

    Se inicia el programa invierteCadenas

---
+++ InvierteCadenas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inviertecadenas;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class InvierteCadenas {
+    public static void main(String[] args) {
+        String cadena = args.length > 0 ? args[0] : "Hola, mundo!";
+        String cadenaInvertida = invertirCadena(cadena);
+        System.out.println("Cadena original: " + cadena);
+        System.out.println("Cadena invertida: " + cadenaInvertida);
+    }
+    public stactic String invertirCadena(String cadena)
+
+}
