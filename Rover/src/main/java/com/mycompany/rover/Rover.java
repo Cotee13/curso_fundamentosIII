b89dc5ad2715c8402edbc9ad0a936372b244e368
Cotee13 <joselbm020@gmail.com>
2026-09-10 12:47:51 -0700

    Actividad Rover

---
+++ Rover/src/main/java/com/mycompany/rover/Rover.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rover;
+
+/**
+ *
+ * @author pinin
+ */
+public class Rover {
+
+    public static void main(String[] args) {
+        String cadena = args.length > 0 ? args[0] : "path1.txt";
+        ArrayList<String> lineas = leerArchivo(cadena);
+        
+        public static AfrayList<String> leerArchivo(String Arraylist<String>) lineas = newArrayList<>());
+        try(BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
+        String linea;
+        while ((linea = br.readLine()) != null {
+        lineas.add(Linea);
+}
+}
+    }
+}
